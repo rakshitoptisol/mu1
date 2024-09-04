d929b032b70f425165f507a590c6c4ddea405125
rakshitoptisol <rakshitroshan@optisolbusiness.com>
2024-09-04 14:30:18 +0530

    first

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,57 @@
-package org.example
+package com.example.api
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
+import org.apache.http.client.methods.CloseableHttpResponse
+import org.apache.http.client.methods.HttpGet
+import org.apache.http.impl.client.CloseableHttpClient
+import org.apache.http.impl.client.HttpClients
+import org.apache.http.util.EntityUtils
+
 fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+    // Create an HttpClient instance
+    val httpClient: CloseableHttpClient = HttpClients.createDefault()
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+    try {
+        // Create a GET request
+        val request = HttpGet("https://jsonplaceholder.typicode.com/posts/1")
+
+        // Execute the request
+        val response: CloseableHttpResponse = httpClient.execute(request)
+
+        // Use 'use' to automatically close resources
+        response.use {
+            // Check response status
+            val statusCode = it.statusLine.statusCode
+            if (statusCode == 200) {
+                // Get response entity
+                val entity = it.entity
+                if (entity != null) {
+                    // Convert response entity to String
+                    val result = EntityUtils.toString(entity)
+                    println(result)
+
+                    // If response is JSON, map it to a Kotlin object
+                    val mapper = jacksonObjectMapper()
+                    val post: Post = mapper.readValue(result)
+                    println(post)
+                }
+            } else {
+                println("Error: $statusCode")
+            }
+        }
+    } catch (e: Exception) {
+        e.printStackTrace()
+    } finally {
+        // Close the HttpClient
+        httpClient.close()
     }
 }
+
+// Data class to map the JSON response
+data class Post(
+    val userId: Int,
+    val id: Int,
+    val title: String,
+    val body: String
+)
